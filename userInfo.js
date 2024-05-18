18d260072fee2d5505da4827fafa00bd100d4e3b
Felix-UG <fekdonkor@st.ug.edu.gh>
2024-05-18 13:05:43 +0100

    Task 3

---
+++ userInfo.js
@@ -0,0 +1,10 @@
+
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => {
+        return {
+            id: index + 1, 
+            originalName: name,
+            modifiedName: modifiedNames[index]
+        };
+    });
+}
